17378ad609e6ae2dce6cb0316500774666402bd9
Frederik Rabøl <frederik-rm@hotmail.com>
2021-08-15 20:42:44 +0200

    added vue grid layout as a plugin, because it doesnt support SSR

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,6 +25,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    {src: '~/plugins/vue-grid-layout', ssr: false},
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
