e250e2670d01a70b951667c61d6a99bff6876e54
Frederik Rabøl <frederik-rm@hotmail.com>
2021-08-22 13:58:59 +0200

    added 404 page

---
+++ pages/_.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>:( page not found</div>
+</template>
+<script lang="ts">
+import { Vue } from 'nuxt-property-decorator';
+export default class NotFound extends Vue {
+}
+</script>
