5ad12a865cc856444b844a4eaf8632b55c3aa10e
Frederik Rabøl <frederik-rm@hotmail.com>
2021-08-22 16:22:19 +0200

    made dashboard pages work

--- pages/dashboards/index.vue
+++ pages/dashboards/index.vue
@@ -1,62 +1,68 @@
 <template>
   <div v-if="$route.query.id">
     <client-only>
-      <NuxtLink :to="{ path: '/dashboards' }">Back</NuxtLink>
-      <!-- <portal to="page-title">Dashboard</portal> -->
-      <!-- <portal to="page-actions"> -->
-      <Button class="primary m-b" @click="editDashboard">Edit</Button>
-      <!-- </portal> -->
-      <grid-layout
-        v-if="getDashboardWidgets"
-        :layout.sync="DashboardWidgets"
-        :cols="defaultSettings.columns"
-        :row-height="defaultSettings.columnHeight"
-        :is-draggable="false"
-        :is-resizable="false"
-        :margin="defaultSettings.margin"
-        :use-css-transforms="true"
-        :responsive="true"
-      >
-        <grid-item
-          v-for="item in DashboardWidgets"
-          :key="item.i"
-          :i="item.i"
-          :w="item.w"
-          :h="item.h"
-          :x="item.x"
-          :y="item.y"
-          :min-w="item.minW || 1"
-          :min-h="item.minH || 1"
-          :max-w="item.maxW || Infinity"
-          :max-h="item.maxH || Infinity"
+      <template v-if="currentBoard">
+        <NuxtLink :to="{ path: '/dashboards' }">Back</NuxtLink>
+        <!-- <portal to="page-title">Dashboard</portal> -->
+        <!-- <portal to="page-actions"> -->
+        <Button class="primary m-b" @click="editDashboard">Edit</Button>
+        <!-- </portal> -->
+        <grid-layout
+          v-if="getDashboardWidgets"
+          :layout.sync="DashboardWidgets"
+          :cols="defaultSettings.columns"
+          :row-height="defaultSettings.columnHeight"
+          :is-draggable="false"
+          :is-resizable="false"
+          :margin="defaultSettings.margin"
+          :use-css-transforms="true"
+          :responsive="true"
         >
-          <widget-wrapper>
-            <component
-              :is="item.type"
-              :settings="item.settings"
-              :dimensions="{ w: item.w, h: item.h }"
-            ></component>
-          </widget-wrapper>
-        </grid-item>
-      </grid-layout>
+          <grid-item
+            v-for="item in DashboardWidgets"
+            :key="item.i"
+            :i="item.i"
+            :w="item.w"
+            :h="item.h"
+            :x="item.x"
+            :y="item.y"
+            :min-w="item.minW || 1"
+            :min-h="item.minH || 1"
+            :max-w="item.maxW || Infinity"
+            :max-h="item.maxH || Infinity"
+          >
+            <widget-wrapper>
+              <component
+                :is="item.type"
+                :settings="item.settings"
+                :dimensions="{ w: item.w, h: item.h }"
+              ></component>
+            </widget-wrapper>
+          </grid-item>
+        </grid-layout>
+      </template>
+      <template v-else>
+        Board not found :(
+      </template>
     </client-only>
   </div>
   <div v-else>
     Dashboard List
-    <Card v-for="id in 10" :key="id">
-      <template #title> Dashboard {{ id }} </template>
+    <client-only>
+    <Card v-for="(board, index) in boards" :key="board.name">
+      <template #title> {{board.name }}</template>
       <template #action>
-        <Button class="primary" @click="link(id)">Dashboard {{ id }}</Button>
+        <Button class="primary" @click="link(index)">view</Button>
       </template>
     </Card>
+    </client-only>
   </div>
 </template>
 <script lang="ts">
-import { Component, Vue } from 'nuxt-property-decorator'
+import { Component, Vue, Watch } from 'nuxt-property-decorator'
 import { defaultSettings } from '@/constants/dashboard'
 import { Button } from '@/components/atoms'
 import { Card } from '@/components/molecules'
-import { widgetsLocalStorageKey } from '~/constants/widgets'
 import { Widget } from '~/types/widgets'
 import {
   Widget as WidgetWrapper,
@@ -66,6 +72,7 @@ import {
   TodoList,
   RejseplanenDeparture,
 } from '@/components/widgets'
+import { Board } from '~/types/dashboards'
 // import HueBridges from '@/components/widgets/hue/bridges.vue';
 // import HueLight from '@/components/widgets/hue/light.vue';
 // import HueGroup from '@/components/widgets/hue/group/group.vue';
@@ -90,32 +97,42 @@ export default class Dashboard extends Vue {
 
   DashboardWidgets: any = null
 
-  created() {
-    this.DashboardWidgets = this.getDashboardWidgets()
+  get boards() {
+    return this.$store.getters['userSettings/boards'];
+  }
+
+  get currentBoard(): Board | null {
+    const currentBoard = this.$route.query.id as string;
+      if(!currentBoard) {
+        return null;
+      }
+      return this.boards[currentBoard];
+  }
+
+  mounted() {
+    this.loadWidgetsBasedOnId(this.currentBoard);
+  }
+
+  @Watch('currentBoard')
+  loadWidgetsBasedOnId(currentBoard: Board | null) {
+    this.DashboardWidgets = this.getDashboardWidgets(currentBoard?.widgets);
   }
 
   // eslint-disable-next-line class-methods-use-this
-  getDashboardWidgets() {
-    let widgets = []
-    if (!process.browser) {
-      return null
+  getDashboardWidgets(widgets: Board[] | undefined) {
+    if (!process.browser || !widgets) {
+      return undefined
     }
-    if (localStorage.getItem(widgetsLocalStorageKey)) {
-      const parsedWidgets = JSON.parse(
-        localStorage.getItem(widgetsLocalStorageKey) || ''
-      )
-      if (typeof parsedWidgets === 'object') {
-        widgets = parsedWidgets
-        widgets.map((widget: Widget) => {
-          const widgetData = { ...widget }
-          if (!widget.guid) {
-            widgetData.guid = new Date().toUTCString()
-          }
-          return widgetData
-        })
+
+    // to make sure we dont override the store, we copy it.
+    const clonedWidgets = JSON.parse(JSON.stringify(widgets)) as Widget[];
+    return clonedWidgets.map((widget) => {
+      const widgetData = { ...widget }
+      if (!widget.guid) {
+        widgetData.guid = new Date().toUTCString()
       }
-    }
-    return widgets
+      return widgetData
+    });
   }
 
   editDashboard() {
--- store/themes.ts
+++ store/themes.ts
@@ -1,76 +1,98 @@
-import { Commit, Dispatch } from "vuex";
-import { themeStorageKey } from "@/constants/themes";
-import { Theme } from "~/types/themes";
+import { Commit, Dispatch } from 'vuex'
+import { themeStorageKey } from '@/constants/themes'
+import { Theme } from '~/types/themes'
 
 interface State {
-	theme: string;
-	themes: Theme[];
+  theme: string
+  themes: Theme[]
 }
 
 export default {
-	state: {
-		theme: "light",
-		themes: [
-			{ name: "dark", icon: "moon", variables: {
-				'--accent-0': '#1D2733',
-				'--text-color': 'var(--white)',
-				'--accent-50': '#222D3B',
-			} },
-			{ name: "light", icon: "sun" },
-			{ name: "red", icon: "star", variables: {
-				'--accent-0': '#475c7a',
-				'--text-color': 'var(--white)',
-				'--accent-50': '#685d79',
-			} },
-			{ name: "green", icon: "tree", variables: {
-				'--accent-0': '#305f72',
-				'--text-color': 'var(--white)',
-				'--accent-50': '#568ea6',
-			} },
-			{ name: "tornado", icon: "tornado", variables: {
-				'--accent-0': '#292929',
-				'--primary': '#c0184d',
-				'--secondary': '#5a9f9e',
-				'--text-color': 'var(--white)',
-				'--accent-50': '#232323',
-				'--success': 'var(--primary)',
-			} },
-		],
-	} as State,
+  state: () =>
+    ({
+      theme: 'light',
+      themes: [
+        {
+          name: 'dark',
+          icon: 'moon',
+          variables: {
+            '--accent-0': '#1D2733',
+            '--text-color': 'var(--white)',
+            '--accent-50': '#222D3B',
+          },
+        },
+        { name: 'light', icon: 'sun' },
+        {
+          name: 'red',
+          icon: 'star',
+          variables: {
+            '--accent-0': '#475c7a',
+            '--text-color': 'var(--white)',
+            '--accent-50': '#685d79',
+          },
+        },
+        {
+          name: 'green',
+          icon: 'tree',
+          variables: {
+            '--accent-0': '#305f72',
+            '--text-color': 'var(--white)',
+            '--accent-50': '#568ea6',
+          },
+        },
+        {
+          name: 'tornado',
+          icon: 'tornado',
+          variables: {
+            '--accent-0': '#292929',
+            '--primary': '#c0184d',
+            '--secondary': '#5a9f9e',
+            '--text-color': 'var(--white)',
+            '--accent-50': '#232323',
+            '--success': 'var(--primary)',
+          },
+        },
+      ],
+    } as State),
 
-	getters: {
-		theme: (state: State) => state.theme,
-		themeIcon: (state: State) => {
-			const foundTheme = state.themes.find((theme) => theme.name === state.theme);
-			return foundTheme ? foundTheme.icon : "";
-		},
-		themes: (state: State) => state.themes,
-	},
+  getters: {
+    theme: (state: State) => state.theme,
+    themeIcon: (state: State) => {
+      const foundTheme = state.themes.find(
+        (theme) => theme.name === state.theme
+      )
+      return foundTheme ? foundTheme.icon : ''
+    },
+    themes: (state: State) => state.themes,
+  },
 
-	actions: {
-		setTheme: ({ commit }: { commit: Commit }, theme: string) => {
-			commit("SET_THEME", theme);
-		},
-		loadTheme: ({ dispatch }: { dispatch: Dispatch }) => {
-			if(!process.browser) {
-				return null;
-			}
-			const localTheme = localStorage.getItem(themeStorageKey);
-			let theme: string | null = localTheme ? JSON.parse(localTheme) : null;
-			if(theme === null && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-				theme = 'dark';
-			}
-			if(theme === null ) {
-				theme = 'light';
-			}
-			dispatch('setTheme', theme);
-		}
-	},
+  actions: {
+    setTheme: ({ commit }: { commit: Commit }, theme: string) => {
+      commit('SET_THEME', theme)
+    },
+    loadTheme: ({ dispatch }: { dispatch: Dispatch }) => {
+      if (!process.browser) {
+        return null
+      }
+      const localTheme = localStorage.getItem(themeStorageKey)
+      let theme: string | null = localTheme ? JSON.parse(localTheme) : null
+      if (
+        theme === null &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches
+      ) {
+        theme = 'dark'
+      }
+      if (theme === null) {
+        theme = 'light'
+      }
+      dispatch('setTheme', theme)
+    },
+  },
 
-	mutations: {
-		SET_THEME: (state: State, theme: string) => {
-			state.theme = theme;
-			localStorage.setItem(themeStorageKey, JSON.stringify(theme));
-		},
-	},
-};
+  mutations: {
+    SET_THEME: (state: State, theme: string) => {
+      state.theme = theme
+      localStorage.setItem(themeStorageKey, JSON.stringify(theme))
+    },
+  },
+}
