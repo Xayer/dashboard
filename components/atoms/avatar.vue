f4aab282ca0ce99d85c07e6f77ef278d92762d60
Frederik Rabøl <frederik-rm@hotmail.com>
2021-08-23 21:34:11 +0200

    added rounded corners

--- components/atoms/avatar.vue
+++ components/atoms/avatar.vue
@@ -1,23 +1,32 @@
 <template>
-    <div>
-        <img v-if="avatar" :src="avatar" />
-    </div>
+  <div>
+    <img v-if="avatar" :src="avatar" />
+  </div>
 </template>
 <script lang="ts">
 import { Component, Prop, Vue } from 'nuxt-property-decorator'
 import { mapGetters } from 'vuex'
 @Component({
-    computed: {
-        ...mapGetters({
-            'avatar' : 'userSettings/avatar'
-        })
-    }
+  computed: {
+    ...mapGetters({
+      avatar: 'userSettings/avatar',
+    }),
+  },
 })
 export default class Avatar extends Vue {
-    @Prop({ default: 48}) size!: number;
-    get avatar() {
-        const image = this.$store.getters['userSettings/avatar']; 
-        return image ? `${image}?s=${this.size}` : undefined
-    }
+  @Prop({ default: 48 }) size!: number
+  get avatar() {
+    const image = this.$store.getters['userSettings/avatar']
+    return image ? `${image}?s=${this.size}` : undefined
+  }
 }
 </script>
+<style lang="scss" scoped>
+img {
+  border-radius: var(--radius);
+}
+div {
+  display: flex;
+  align-items: center;
+}
+</style>
