961d95df9898dff47d18922f63cb96d906a84e1d
Frederik Rabøl <frederik-rm@hotmail.com>
2021-12-07 17:32:10 +0100

    new favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
       {
         rel: 'stylesheet',
         href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@300;600;700&display=swap',
