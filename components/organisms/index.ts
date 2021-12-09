a3046675a75964eb88caff5e08e86ca9e95bd011
Frederik Rabøl <frederik-rm@hotmail.com>
2021-12-09 20:43:55 +0100

    fixed renaming bug

--- components/organisms/index.ts
+++ components/organisms/index.ts
@@ -1,2 +1,2 @@
-export { default as Header } from '@/components/organisms/Header.vue'
+export { default as Header } from '@/components/organisms/MainHeader.vue'
 export { default as DashboardList } from '@/components/organisms/DashboardList.vue'
